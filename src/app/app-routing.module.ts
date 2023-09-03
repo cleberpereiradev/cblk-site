baafa978960d42f60fcda8a333e1498205f2e609
cleberpereiradev <cleberpereiraeng@gmail.com>
2023-09-03 11:54:54 -0300

    <feat> Includes game, games components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { GamesComponent } from './pages/games/games/games.component';
+import { GameComponent } from './pages/games/game/game.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'home', component: HomeComponent},
-  {path: 'personal', component: GamesComponent}
+  {path: 'games', component: GamesComponent},
+  {path: 'game', component: GameComponent},
 ];
 
 
